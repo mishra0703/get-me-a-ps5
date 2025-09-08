683f6d802a54f86938f499a4b182a0b94eba85c7
Prem Mishra <premmishra874478@gmail.com>
2025-09-09 00:15:42 +0530

    first commit

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/fundRaiser", {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI, {
       useNewUrlParser: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
